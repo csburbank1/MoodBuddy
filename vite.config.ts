e05907e6e80971d19b79c79c18ede314580bc3e4
csburbank1 <chburbank@rochesterschools.org>
2025-04-11 13:45:10 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
   },
 });
